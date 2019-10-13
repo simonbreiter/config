b217133c79a72b8615392e1aaad957bb1c0297c0
Simon Breiter <hello@simonbreiter.com>
2019-10-13 10:18:28 +0000

    Update .init.sh

--- .init.sh
+++ .init.sh
@@ -43,7 +43,7 @@ installHomebrewApps()
   gdb \
   ranger \
   ngrep \
-  pstree \
+  pstree;
   echo "set startup-with-shell off" >> ~/.gdbinit
 
   echo "Installing homebrew cask applications...";
